eee16346826ab61eb42a45b76608013600a05650
Eric Winkler <winkler.em@gmail.com>
2012-08-30 16:35:22 -0400

    Reintroduced v1 compatibility urls at top layer

--- src/sana/urls.py
+++ src/sana/urls.py
@@ -4,13 +4,14 @@
 :Authors: Sana dev team
 :Version: 2.0
 """
-import sys
 
 from django.conf import settings
 from django.conf.urls.defaults import patterns, url, include
+from django.views.generic.simple import redirect_to
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
+from sana.api.v1.v2compatlib import redirect_to_v1
 
 admin.autodiscover()
 
@@ -19,6 +20,7 @@ urlpatterns = patterns(
     url(r'^$', 'sana.core.views.home', name="home"),
     url(r'^core/', include('sana.core.urls', namespace='core')),
     url(r'^mds/', include('sana.mds.urls', namespace='mds')),
+    url(r'^mrs/', include('sana.mrs.urls', namespace='mrs')),
     # ADMIN
     (r'^admin/', include(admin.site.urls)),
 )
@@ -30,3 +32,76 @@ if settings.DEBUG:
     urlpatterns += patterns('django.contrib.staticfiles.views',
         url(r'^static/(?P<path>.*)$', 'serve'),
     )
+    
+v1patterns = patterns(
+    '',
+    
+    url(r'^notifications/$',
+        redirect_to,
+        {'url': '/mrs/notifications/'},
+        name="sana-list-notifications-redirect"),
+
+    url(r'^notifications/submit/$',
+        redirect_to,
+        {'url': '/mrs/notifications/submit/'},
+        name="sana-api-notification-submit-redirect"),
+
+    url(r'^notifications/submit/email/$',
+        redirect_to,
+        {'url': '/mrs/notifications/submit/email/'},
+        name="sana-api-email-notification-submit-redirect"),
+
+     url(r'^json/patient/list/$',
+        redirect_to,
+        {'url': '/mrs/json/patient/list/'},
+         name="sana-json-patient-list-redirect"),
+
+     url(r'^json/patient/(?P<id>[0-9-]+)/$',
+        redirect_to,
+        {'url': '/mrs/json/patient/(?P<id>[0-9-]+)/'},
+         name="sana-json-patient-get-redirect"),
+
+
+    url(r'^json/validate/credentials/$',
+        redirect_to,
+        {'url': '/mrs/json/validate/credentials/',},
+        name = "sana-json-validate-credentials-redirect"),
+
+    url(r'^procedure/submit/$',
+        redirect_to,
+        {'url': '/mrs/json/validate/credentials/'},
+        name="sana-html-procedure-submit-redirect"),
+
+    url(r'^json/procedure/submit/$',
+        redirect_to,
+        {'url': ''},
+        name="sana-json-procedure-submit-redirect"),
+
+    url(r'^json/binary/submit/$',
+        redirect_to,
+        {'url': '/mrs/json/binary/submit/'},
+        name="sana-json-binary-submit-redirect"),
+
+    url(r'^json/binarychunk/submit/$',
+        redirect_to,
+        {'url': '/mrs/json/binarychunk/submit/'},
+        name="sana-json-binarychunk-submit-redirect"),
+
+    url(r'^json/textchunk/submit/$',
+        redirect_to,
+        {'url': '/mrs/json/textchunk/submit/'},
+        name="sana-json-binarychunk-hack-submit-redirect"),
+
+    url(r'^json/eventlog/submit/$',
+        redirect_to,
+        {'url': '/mrs/json/eventlog/submit/'},
+        name="sana-json-eventlog-submit-redirect"),
+)
+
+_compats = {'v1': v1patterns }
+
+if settings.COMPAT_URLS:
+    for compat in settings.COMPAT_URLS:
+        urlpatterns += _compats[compat]
+        
+    
