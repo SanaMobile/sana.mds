98483964be8698810c21ad22dde31dcaef3bb885
Eric Winkler <winkler.em@gmail.com>
2014-08-22 16:46:38 -0400

    Refined the admin pages for the models.
    Added the 'mark voided' functionality.

--- src/mds/core/admin.py
+++ src/mds/core/admin.py
@@ -7,14 +7,20 @@
 from django.contrib import admin
 from .models import * 
 
-class ProcedureAdmin(admin.ModelAdmin):
-    pass
+def mark_voided(modeladmin,request,queryset):
+    queryset.update(voided=True)
+mark_voided.short_description = "Mark selected voided"
 
-class ClientEventLogAdmin(admin.ModelAdmin):
-    list_display = ('client', 'event_time', 'event_type', 'event_value', 'encounter_reference', 'patient_reference', 'user_reference',)
-    list_filter = ('event_time', 'event_type', 'encounter_reference', 'patient_reference', 'user_reference',)
-    date_hierarchy = 'event_time'
-    exclude = ('created', 'modified',)
+class DeviceAdmin(admin.ModelAdmin):
+    readonly_fields = ['uuid']  
+    list_display = ['name', 'uuid']
+    list_filter = ['name',]
+    actions=[mark_voided,]
+
+class ProcedureAdmin(admin.ModelAdmin):
+    readonly_fields = ['uuid']
+    list_display = ['title', 'author', 'uuid']
+    actions=[mark_voided,]
 
 class RestAdmin(admin.TabularInline):
     app_label="REST Services"
@@ -25,21 +31,48 @@ class RelationshipAdmin(admin.TabularInline):
     fk_name = 'to_concept'
     list_display_links = []
     
-    
 class ConceptAdmin(admin.ModelAdmin):
     inlines = [ 
         RelationshipAdmin,
         ]
     readonly_fields = ['uuid']  
     list_display = ['name', 'uuid']
-    
-class ObservationAdmin(admin.StackedInline):
-    model = Observation
+    list_filter = ['name',]
+    actions=[mark_voided,]
+
+class ObservationAdmin(admin.ModelAdmin):
+    exclude = ('_complex_progress',)
+    readonly_fields = ['_complex_size','uuid','value']
+    list_display = ['question','voided','concept','value', 
+        'subject','device','created','modified', 'encounter', 'upload_progress']
+    list_filter = ['node','concept', 'modified', 'encounter']
+    actions=[mark_voided,]
 
 class EncounterAdmin(admin.ModelAdmin):
-    inlines = [ 
-        ObservationAdmin,
-        ]  
+    exclude = ['concept',]
+    list_display = ['subject','voided','procedure', 'created','uuid',"observer",]
+    actions = [mark_encounter_voided,]
+    actions=[mark_voided,]
+
+class EncounterInline(admin.StackedInline):
+    model = Encounter
+
+class ObserverAdmin(admin.ModelAdmin):
+    readonly_fields = ['uuid',]
+    list_display = ['user', 'uuid']
+    actions=[mark_voided,]
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    readonly_fields = ['uuid',]
+    list_display = ['system_id','given_name', 'family_name', 'uuid', "image"]
+
+class SubjectInline(admin.StackedInline):
+    model = Subject
+
+class SurgicalSubjectAdmin(admin.ModelAdmin):
+    readonly_fields = ['uuid',]
+    list_display = ['system_id','given_name', 'family_name', 'uuid', "image"]
 
 class LocationAdmin(admin.ModelAdmin):
     model = Location
@@ -49,14 +82,12 @@ class LocationAdmin(admin.ModelAdmin):
 admin.site.register(Concept, ConceptAdmin)
 admin.site.register(Relationship)
 admin.site.register(RelationshipCategory)
-admin.site.register(Device)
+admin.site.register(Device, DeviceAdmin)
 admin.site.register(Encounter, EncounterAdmin)
+admin.site.register(Observation,ObservationAdmin)
 admin.site.register(Location,LocationAdmin)
 admin.site.register(Notification)
-admin.site.register(Observation)
-admin.site.register(Observer)
-admin.site.register(Procedure)
-admin.site.register(Subject)
+admin.site.register(Observer,ObserverAdmin)
+admin.site.register(Procedure,ProcedureAdmin)
+admin.site.register(Subject,SubjectAdmin)
 admin.site.register(Event)
-
-#admin.site.register(ClientEventLog, ClientEventLogAdmin)
