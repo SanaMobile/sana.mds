bd09af7a181555762698ac88cc67d4d1ff8f7556
ewinkler <winkler.em@gmail.com>
2015-09-12 13:08:30 -0400

    Added exception catch

--- src/mds/api/v1/api.py
+++ src/mds/api/v1/api.py
@@ -335,9 +335,11 @@ def maybe_upload_procedure(saved_procedure):
         for obs in observations:
             obs.save()
         if result:
-            #if obs.concept.is_complex:
-            notification_sent = sender.send_review_notification(encounter)
-            logging.info("Sent notification for Observation: %s" % encounter.uuid)
+            try:
+                notification_sent = sender.send_review_notification(encounter)
+                logging.info("Sent notification for Observation: %s" % encounter.uuid)
+            except:
+                logging.warn("Problem sending notifications")
     return result, message
 
 def register_binary(procedure_guid, element_id, data):
