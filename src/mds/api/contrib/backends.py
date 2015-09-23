f89603171a9143ad41603732f07b49b77b71478d
ewinkler <winkler.em@gmail.com>
2015-09-23 17:27:05 -0400

    Update to the abstract backend handler.
    This lays out a further unified structure for the handler
    to process the models in the core.
    
    TODO: Integrate task models.

--- src/mds/api/contrib/backends.py
+++ src/mds/api/contrib/backends.py
@@ -18,29 +18,185 @@ __all__ = [
 ]
 _handlers = {
     'Concept': [],
+    'RelationShip': [],
+    'RelationshipCategory':[],
+    'Device': [],
     'Encounter': [],
+    'Event': [],
+    'Instruction': [],
     'Location':[],
+    'Notification': [],
     'Procedure':[],
     'Observation':[],
     'Observer'[],
+    'Procedure': [],
     'Subject':[],
 }
 
 class AbstractHandler(object)
-    def __init__(self, **kwargs):
+    def __init__(self, model, **kwargs):
         pass
     
-    def create_model(self):
-        pass
+    def create_concept(self):
+        return None
 
-    def read_model(self):
-        pass
+    def create_relationship(self):
+        return None
+    
+    def create_relationshipcategory(self):
+        return None
+    
+    def create_device(self):
+        return None
+    
+    def create_encounter(self):
+        return None
+    
+    def create_event(self):
+        return None
+    
+    def create_instruction(self):
+        return None
+    
+    def create_location(self):
+        return None
+    
+    def create_notification(self):
+        return None
+    
+    def create_observation(self):
+        return None
+    
+    def create_observer(self):
+        return None
+    
+    def create_procedure(self):
+        return None
+    
+    def create_subject(self):
+        return None
+    
+    # Read functions
+    def read_concept(self):
+        return None
+    
+    def read_relationship(self):
+        return None
+    
+    def read_relationshipcategory(self):
+        return None
+    
+    def read_device(self):
+        return None
+    
+    def read_encounter(self):
+        return None
+    
+    def read_event(self):
+        return None
+    
+    def read_instruction(self):
+        return None
+    
+    def read_location(self):
+        return None
+    
+    def read_notification(self):
+        return None
+    
+    def read_observation(self):
+        return None
+    
+    def read_observer(self):
+        return None
+    
+    def read_procedure(self)
+        return None:
+    
+    def read_subject(self):
+        return None
 
-    def update_model(self):
-        pass
+    # update methods
+    def update_concept(self):
+        return None
+    
+    def update_relationship(self):
+        return None
+    
+    def update_relationshipcategory(self):
+        return None
+    
+    def update_device(self):
+        return None
+    
+    def update_encounter(self):
+        return None
+    
+    def update_event(self):
+        return None
+    
+    def update_instruction(self):
+        return None
+    
+    def update_location(self):
+        return None
+    
+    def update_notification(self):
+        return None
+    
+    def update_observation(self):
+        return None
+    
+    def update_observer(self):
+        return None
+    
+    def update_procedure(self)
+        return None:
+    
+    def update_subject(self):
+        return None
+    
+    # Delete methods
+    def delete_concept(self):
+        return None
+    
+    def delete_relationship(self):
+        return None
+    
+    def delete_relationshipcategory(self):
+        return None
+    
+    def delete_device(self):
+        return None
+    
+    def delete_encounter(self):
+        return None
+    
+    def delete_event(self):
+        return None
+    
+    def delete_instruction(self):
+        return None
+    
+    def delete_location(self):
+        return None
+    
+    def delete_notification(self):
+        return None
+    
+    def delete_observation(self):
+        return None
+    
+    def delete_observer(self):
+        return None
+    
+    def delete_procedure(self)
+        return None:
+    
+    def delete_subject(self):
+        return None
+    
 
-    def delete_model(self):
-        pass
 
 def register_handler(model, target):
     pass
@@ -68,7 +224,7 @@ def create(model, obj, auth=None, *args, **kwargs):
         logging.warn("No handler defined for %s" % model) 
     for handler in handlers:
         handler_instance = handler(auth=auth)
-        caller = handler.getattr('create_model', None)
+        caller = handler.getattr('create_%s' % model.lower(), None)
         if caller:
             if result:
                 caller(obj,*args,**kwargs)
@@ -92,7 +248,7 @@ def read(model, obj, auth=None, *args, **kwargs):
         logging.warn("No handler defined for %s" % model) 
     for handler in handlers:
         handler_instance = handler(auth=auth)
-        caller = handler.getattr('read_model', None)
+        caller = handler.getattr('read_%s' % model.lower(), None)
         if caller:
             if result:
                 caller(obj,*args,**kwargs)
@@ -116,7 +272,7 @@ def update(model, obj, auth=None, *args, **kwargs):
         logging.warn("No handler defined for %s" % model) 
     for handler in handlers:
         handler_instance = handler(auth=auth)
-        caller = handler.getattr('update_model', None)
+        caller = handler.getattr('update_%s' % model.lower(), None)
         if caller:
             if result:
                 caller(obj,*args,**kwargs)
@@ -140,7 +296,7 @@ def delete(model, obj, auth=None, *args, **kwargs):
         logging.warn("No handler defined for %s" % model) 
     for handler in handlers:
         handler_instance = handler(auth=auth)
-        caller = handler.getattr('delete_model', None)
+        caller = handler.getattr('delete_%s' % model.lower(), None)
         if caller:
             if result:
                 caller(obj,*args,**kwargs)
