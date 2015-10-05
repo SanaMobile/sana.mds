4e7a3892d28f52cba74b501d6c73531cfa571e56
ewinkler <winkler.em@gmail.com>
2015-10-05 13:09:41 -0400

    Fix to email sending.

--- src/mds/api/contrib/smtplib/sender.py
+++ src/mds/api/contrib/smtplib/sender.py
@@ -7,42 +7,42 @@ import urllib
 import logging
 
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import send_mail, mail_admins
 
 __all__ = ["send_review_notification",]
-
-#TODO These should be saved as settings or a global config table
-link = "https://%s/mds/web/form/surgeon/review/%s/"
-#mobile_link = "https://%s/mds/core/mobile/%s/%s/"
 
 def send_review_notification(instance,addresses,subject,
     review_host=settings.REVIEW_HOSTNAME, 
     replyTo=settings.SMTP_REPLYTO,
     template=settings.REVIEW_POST_TEMPLATE,
+    review_link=settings.REVIEW_URL_TEMPLATE,
     auth_user=settings.EMAIL_HOST_USER,
     auth_password=settings.EMAIL_HOST_PASSWORD):
     """ Formats and sends and email message which include a url for reviewing
         an uploaded encounter.
     """
+    result = True
     try:
-        url = link % (
-            review_host,
-            instance.uuid
-        )
-        logging.debug("review link %s" % url)
-        #urlencodred = urllib.urlencode(url)
-        #print urlencoded)
-        message = template % (url)
-        logging.debug("Review message:\n %s" % message)
-        send_mail(subject, message, settings.SMTP_REPLYTO,addresses, 
+        url = review_link.format(
+            review_host=review_host,
+            uuid=instance.uuid
+            )
+        message = template.format(url=url)
+        # Send one at a time here
+        for address in addresses:
+            try:
+                send_mail(subject, message, settings.SMTP_REPLYTO,
+                    [address], 
                     fail_silently=False,
                     auth_user=auth_user, 
                     auth_password=auth_password)
-        for address in addresses:
-            print address
-            logging.info("Review notification sent successfully to %s" % address)
-        result = True
+                logging.info("Notification sent successfully %s" % address)
+            except:
+                fail_msg = "Notification send failed! {address}".format(address=address)
+                logging.warn(fail_msg)
+                result = False
+                mail_admins("Server Error", fail_message, fail_silently=True)
     except:
-        logging.error("Review notification send failed!")
+        logging.error("Error formatting notification message!")
         result = False
     return result
