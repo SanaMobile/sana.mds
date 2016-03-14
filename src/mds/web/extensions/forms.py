119d990b7579b3174f801b4e61f0d1d0fee1f4e5
ewinkler <winkler.em@gmail.com>
2016-03-14 15:16:56 -0400

    Forms for querying

--- src/mds/web/extensions/forms.py
+++ src/mds/web/extensions/forms.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django import forms
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from extra_views import ModelFormSetView
 
@@ -41,3 +44,80 @@ class SurgicalAdvocateFollowUpForm(forms.Form):
     date_of_final_sa_follow_up = forms.DateTimeField(
             widget=DateSelectorInput())
 
+class QueryForm(forms.Form):
+
+    def qfilter(self):
+        squery = {}
+        if self.is_valid():
+            for k,v in self.cleaned_data.items():
+                if v:
+                    squery[k] = v
+        return squery
+
+class EncounterQueryForm(forms.Form):
+    observer = forms.ModelChoiceField(queryset=SurgicalAdvocate.objects.all())
+    start_date = forms.DateTimeField(
+            label=_('After'),
+            widget=DateSelectorInput())
+    end_date = forms.DateTimeField(
+            label=('Before'),
+            widget=DateSelectorInput())
+    operation = forms.ChoiceField(choices=settings.ALLOWED_OPERATIONS)
+    gender = forms.ChoiceField(label=_('Gender'), choices=settings.GENDER_CHOICES)
+    min_age = forms.IntegerField(label=_('Min age'), min_value=0)
+    max_age = forms.IntegerField(label=_('Max age'), max_value=120)
+    given_name = forms.CharField(label=_('Given Name'))
+    family_name = forms.CharField(label=_('Family Name'))
+
+    def qfilter(self):
+        datefmt = '%Y-%m-%d'
+        squery = {}
+        if self.is_valid():
+            data = self.cleaned_data
+            # Subject query filters
+            system_id = data.get('system_id',None)
+            if system_id:
+                squery['subject__system_id__exact'] = system_id
+            if given_name:
+                squery["subject__given_name__contains"] = given_name
+            family_name = data.get('family_name',None)
+            if family_name:
+                squery["subject__family_name__contains"] = family_name
+            gender = data.get('gender',None)
+            if gender:
+                squery["subject__gender__exact"] = gender
+            
+            now = datetime.datetime.now()
+            min_age = data.get('min_age', None)
+            if min_age:
+                squery["subject__dob__gte"] = min_age
+            max_age = data.get('max_age', None)
+            if max_age:
+                squery["subject__dob__lte"] = given_name
+                
+            # get start and end date strings
+            start_str = data.get('start', None)
+            end_str = data.get('end', None)
+    
+            # set the end datetime
+            if end_str:
+                end_date = datetime.datetime.strptime(end_str[:10],datefmt)
+            else:
+                end_date = now.replace(day=1) - datetime.timedelta(days=1)
+                
+            # set the start datetime
+            if start_str:
+                start_date = datetime.datetime.strptime(start_str[:10],datefmt )
+            elif end_str:
+                start_date = end_date - datetime.timedelta(days=30)
+            else:
+                start_date = end_date.replace(day=1)
+            squery['created__range']=[start_date, end_date]
+        return squery
+
+class SubjectQuery(QueryForm):
+    gender = forms.ChoiceField(label=_('Gender'), choices=settings.GENDER_CHOICES)
+    min_age = forms.IntegerField(label=_('Min age'), min_value=0, max_value=120)
+    max_age = forms.IntegerField(label=_('Max age'), min_value=0, max_value=120)
+    given_name = forms.CharField(label=_('Given Name'))
+    family_name = forms.CharField(label=_('Family Name'))
