b2eab17a5ddfd1dee70d5ecd503085cb2eb3a62d
ewinkler <winkler.em@gmail.com>
2016-03-14 15:15:31 -0400

    Settings for web app

---
+++ src/mds/web/settings.py
@@ -0,0 +1,20 @@
+from django.utils.translation import ugettext_lazy as _
+
+ALLOWED_OPERATIONS = (
+        ("Inguinal Hernia Repair",_("Inguinal Hernia Repair")),
+        ("Other Hernia Repair",_("Other Hernia Repair")),
+        ("Breat Biopsy",_("Breat Biopsy")),
+        ("Mastectomy",_("Mastectomy")),
+        ("Mastectomy+Axillary LN Dissection",_("Mastectomy+Axillary LN Dissection")),
+        ("Mastectomy+Axillary LN Biospy",_("Mastectomy+Axillary LN Biospy")),
+        ("Biopsy, other",_("Biopsy, other")),
+        ("Excision of Mass other than breast",_("Excision of Mass other than breast")),
+        ("Other Operation", _("Other Operation")),
+)
+GENDER_CHOICES = (
+    ("M",_("Male")),
+    ("F",_("Female"))
+)
+INTAKE_CONCEPTS = (
+    '266d8a8f-9178-4616-bcea-896194379059',
+)
