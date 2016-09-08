a8367721445a535fd2201d5af46da521ea6330df
ewinkler <winkler.em@gmail.com>
2016-09-08 14:40:31 -0400

    Forms for project models

--- src/mds/core/extensions/forms.py
+++ src/mds/core/extensions/forms.py
@@ -4,4 +4,19 @@ Project specific extensions to core forms
 :Authors: Sana Dev Team
 :Version: 2.0
 '''
-__all__ = []
+
+from django import forms
+from .models import ANM, Patient
+
+__all__ = [
+    'ANMForm'
+    'PatientForm'
+    ]
+
+class ANMForm(forms.ModelForm):
+    class Meta:
+        model = ANM
+        
+class PatientForm(forms.ModelForm):
+    class Meta:
+        model = Patient
