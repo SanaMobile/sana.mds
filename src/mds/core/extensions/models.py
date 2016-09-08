795a4c010c7fd2731d66bb4257fea72c3970abce
ewinkler <winkler.em@gmail.com>
2016-09-08 14:40:12 -0400

    Project models

--- src/mds/core/extensions/models.py
+++ src/mds/core/extensions/models.py
@@ -4,3 +4,36 @@ Extensions for core models
 :Authors: Sana dev team
 :Version: 2.0
 """
+from django.db import models
+
+from mds.core.models.encounter import Encounter
+from mds.core.models.location import Location
+from mds.core.models.observer import Observer
+from mds.core.models.subject import Subject
+
+
+__all__ = ["ANM","Patient"]
+
+class ANM(Observer):
+    
+    class Meta:
+        app_label = "core"
+        verbose_name = 'ANM'
+    locations = models.ManyToManyField('core.Location', blank=True)
+    """Locations ANM is assigned to"""
+    
+class Patient(Subject):
+    
+    class Meta:
+        app_label = "core"
+        verbose_name = 'patient'
+    secondary_id = models.CharField(max_length=12, blank=True)
+    """AADHAR ID"""
+    
+    caregiver_name = models.CharField(max_length=128, blank=True)
+    """Mother's name"""
+    
+    secondary_caregiver_name = models.CharField(max_length=128, blank=True)
+    """Father's name"""
+
+
