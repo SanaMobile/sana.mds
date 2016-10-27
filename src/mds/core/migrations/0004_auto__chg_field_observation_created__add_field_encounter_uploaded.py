039523666893561b9d65501a7a843b66cb196f69
ewinkler <winkler.em@gmail.com>
2016-10-27 19:36:01 -0400

    Schem migration

---
+++ src/mds/core/migrations/0004_auto__chg_field_observation_created__add_field_encounter_uploaded.py
@@ -0,0 +1,233 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Observation.created'
+        db.alter_column(u'core_observation', 'created', self.gf('django.db.models.fields.DateTimeField')())
+        # Adding field 'Encounter.uploaded'
+        db.add_column(u'core_encounter', 'uploaded',
+                      self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, auto_now_add=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+
+        # Changing field 'Observation.created'
+        db.alter_column(u'core_observation', 'created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True))
+        # Deleting field 'Encounter.uploaded'
+        db.delete_column(u'core_encounter', 'uploaded')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'core.anm': {
+            'Meta': {'object_name': 'ANM', '_ormbases': ['core.Observer']},
+            'locations': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['core.Location']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'observer_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['core.Observer']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'core.concept': {
+            'Meta': {'object_name': 'Concept'},
+            'conceptclass': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'constraint': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'datatype': ('django.db.models.fields.CharField', [], {'default': "'string'", 'max_length': '64'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'mimetype': ('django.db.models.fields.CharField', [], {'default': "'text/plain'", 'max_length': '64'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'uuid': ('django.db.models.fields.SlugField', [], {'default': "'792893ac-f1ad-47a0-b6fd-068245c94466'", 'unique': 'True', 'max_length': '36'}),
+            'voided': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'core.device': {
+            'Meta': {'object_name': 'Device'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '36'}),
+            'uuid': ('django.db.models.fields.SlugField', [], {'default': "'fd141cea-11b3-4e6b-abc1-8219c0922983'", 'unique': 'True', 'max_length': '36'}),
+            'voided': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'core.encounter': {
+            'Meta': {'object_name': 'Encounter'},
+            'concept': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Concept']", 'to_field': "'uuid'"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'device': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Device']", 'to_field': "'uuid'"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Location']", 'to_field': "'uuid'", 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'observer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Observer']", 'to_field': "'uuid'"}),
+            'procedure': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Procedure']", 'to_field': "'uuid'"}),
+            'subject': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Subject']", 'to_field': "'uuid'"}),
+            'uploaded': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
+            'uuid': ('django.db.models.fields.SlugField', [], {'default': "'787c5afb-e9b4-4da4-9fb6-4fbdaa187c3b'", 'unique': 'True', 'max_length': '36'}),
+            'voided': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'core.event': {
+            'Meta': {'object_name': 'Event'},
+            'client': ('django.db.models.fields.CharField', [], {'max_length': '16'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'duration': ('django.db.models.fields.FloatField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'level': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'messages': ('django.db.models.fields.TextField', [], {}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '767'}),
+            'path': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'uuid': ('django.db.models.fields.SlugField', [], {'default': "'da401789-95e0-4ccd-92c2-4e5442db6124'", 'unique': 'True', 'max_length': '36'})
+        },
+        'core.instruction': {
+            'Meta': {'object_name': 'Instruction'},
+            'algorithm': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'boolean_operator': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'compound': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'concept': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Concept']", 'to_field': "'uuid'"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'predicate': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'uuid': ('django.db.models.fields.SlugField', [], {'default': "'fd17b247-7cc0-4c50-9cd9-a9b370c9f5db'", 'unique': 'True', 'max_length': '36'}),
+            'voided': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'core.location': {
+            'Meta': {'object_name': 'Location'},
+            'code': ('django.db.models.fields.IntegerField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'uuid': ('django.db.models.fields.SlugField', [], {'default': "'94d8b40f-f2fd-4166-8303-4a90b89db376'", 'unique': 'True', 'max_length': '36'})
+        },
+        'core.notification': {
+            'Meta': {'object_name': 'Notification'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '512'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'delivered': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'header': ('django.db.models.fields.CharField', [], {'max_length': '512'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'message': ('django.db.models.fields.TextField', [], {}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'uuid': ('django.db.models.fields.SlugField', [], {'default': "'9a55d98a-fa12-4eb1-8869-8a7f7cc91e70'", 'unique': 'True', 'max_length': '36'}),
+            'voided': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'core.observation': {
+            'Meta': {'ordering': "['-created']", 'unique_together': "(('encounter', 'node'),)", 'object_name': 'Observation'},
+            '_complex_progress': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            '_complex_size': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'concept': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Concept']", 'to_field': "'uuid'"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'encounter': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Encounter']", 'to_field': "'uuid'"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'node': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'uuid': ('django.db.models.fields.SlugField', [], {'default': "'eea091d4-43fa-444d-ad19-6a215074cc82'", 'unique': 'True', 'max_length': '36'}),
+            'value_complex': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'blank': 'True'}),
+            'value_text': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'voided': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'core.observer': {
+            'Meta': {'object_name': 'Observer'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'}),
+            'uuid': ('django.db.models.fields.SlugField', [], {'default': "'b259212d-0c7e-4897-9a09-4249b934a6a1'", 'unique': 'True', 'max_length': '36'}),
+            'voided': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'core.patient': {
+            'Meta': {'object_name': 'Patient', '_ormbases': ['core.Subject']},
+            'caregiver_name': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'secondary_caregiver_name': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'secondary_id': ('django.db.models.fields.CharField', [], {'max_length': '12', 'blank': 'True'}),
+            u'subject_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['core.Subject']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'core.procedure': {
+            'Meta': {'object_name': 'Procedure'},
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'src': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'uuid': ('django.db.models.fields.SlugField', [], {'default': "'3179981a-c3ea-44be-a76b-c191e816b97a'", 'unique': 'True', 'max_length': '36'}),
+            'version': ('django.db.models.fields.CharField', [], {'default': "'1.0'", 'max_length': '255'}),
+            'voided': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'core.relationship': {
+            'Meta': {'object_name': 'Relationship'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.RelationshipCategory']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'from_concept': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'concept_related_from'", 'to_field': "'uuid'", 'to': "orm['core.Concept']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'to_concept': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'concept_related_to'", 'to_field': "'uuid'", 'to': "orm['core.Concept']"}),
+            'uuid': ('django.db.models.fields.SlugField', [], {'default': "'1af22b9c-219e-4258-9dd6-feef5049cd96'", 'unique': 'True', 'max_length': '36'})
+        },
+        'core.relationshipcategory': {
+            'Meta': {'object_name': 'RelationshipCategory'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '512', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'restriction': ('django.db.models.fields.CharField', [], {'max_length': '512', 'blank': 'True'}),
+            'uuid': ('django.db.models.fields.SlugField', [], {'default': "'64609809-31a3-4e56-8788-91210629aa27'", 'unique': 'True', 'max_length': '36'})
+        },
+        'core.subject': {
+            'Meta': {'object_name': 'Subject'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'dob': ('django.db.models.fields.DateTimeField', [], {}),
+            'family_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'gender': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'given_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
+            'location': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['core.Location']", 'to_field': "'uuid'", 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'system_id': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'uuid': ('django.db.models.fields.SlugField', [], {'default': "'3b56b81e-7830-45e0-88d0-85ed3abd3306'", 'unique': 'True', 'max_length': '36'}),
+            'voided': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        }
+    }
+
+    complete_apps = ['core']
