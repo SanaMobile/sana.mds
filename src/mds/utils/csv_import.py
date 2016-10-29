bdeeab65c7e503b0c4a5ef3f8f1d85c1157d5aa0
ewinkler <winkler.em@gmail.com>
2016-10-29 12:49:40 -0400

    Added try catch within parse loop

--- src/mds/utils/csv_import.py
+++ src/mds/utils/csv_import.py
@@ -53,49 +53,53 @@ def load_users(fname):
             command = int(row[0])
             username=row[1]
             # 1 = add users
-            if command == 1:
-                print("Adding user %s" % username)
-                password=row[2]
-                location_str = row[3]
-                locations = []
-                for code in location_str.split(';'):
-                    locations.append(Location.objects.get(code=code))
-                
-                # create User
-                user = User.objects.create_user(username=username, password=password)
-                user.save()
-                # Create ANM
-                anm = ANM(user=user)
-                anm.save()
-                for location in locations:
-                    anm.locations.add(location)
-                anm.save()
-            #2=Modify(add villages),
-            elif command == 2:
-                print("Modifying user %s. Adding villages." % username)
-                anm = ANM.objects.get(user__username=username)
-                location_str = row[2]
-                for location in location_str.split(';'):
-                    anm.locations.add(location)
-                anm.save()
-            # 3=Modify(Replace villages)
-            elif command == 3:
-                print("Modifying user %s. Replacing villages." % username)
-                anm = ANM.objects.get(user__username=username)
-                # remove villages
-                for location in anm.locations.all():
-                    anm.locations.remove(location)
-                anm.save()
-                location_str = row[2]
-                for code in location_str.split(';'):
-                    if not test:
+            index = 1
+            try:
+                if command == 1:
+                    print("Adding user %s" % username)
+                    password=row[2]
+                    location_str = row[3]
+                    locations = []
+                    for code in location_str.split(';'):
+                        locations.append(Location.objects.get(code=code))
+            
+                    # create User
+                    user = User.objects.create_user(username=username, password=password)
+                    user.save()
+                    # Create ANM
+                    anm = ANM(user=user)
+                    anm.save()
+                    for location in locations:
+                        anm.locations.add(location)
+                    anm.save()
+                #2=Modify(add villages),
+                elif command == 2:
+                    print("Modifying user %s. Adding villages." % username)
+                    anm = ANM.objects.get(user__username=username)
+                    location_str = row[2]
+                    for location in location_str.split(';'):
+                        anm.locations.add(location)
+                    anm.save()
+                # 3=Modify(Replace villages)
+                elif command == 3:
+                    print("Modifying user %s. Replacing villages." % username)
+                    anm = ANM.objects.get(user__username=username)
+                    # remove villages
+                    for location in anm.locations.all():
+                        anm.locations.remove(location)
+                    anm.save()
+                    location_str = row[2]
+                    for code in location_str.split(';'):
                         anm.locations.add(Location.objects.get(code=code))
                 anm.save()
-            # 4=remove locations
-            elif command == 4:
-                print("Modifying user %s. Removing villages." % username)
-                anm = ANM(user__username=username)
-                location_str = row[2]
-                for code in location_str.split(';'):
-                    anm.locations.remove(Location.objects.get(code=code))
-                anm.save()
+                # 4=remove locations
+                elif command == 4:
+                    print("Modifying user %s. Removing villages." % username)
+                    anm = ANM(user__username=username)
+                    location_str = row[2]
+                    for code in location_str.split(';'):
+                        anm.locations.remove(Location.objects.get(code=code))
+                    anm.save()
+            except:
+                    print("Line %d. Load failed" % index)
+            index = index + 1
