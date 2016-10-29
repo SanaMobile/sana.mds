f3ca76a57d84d921978490f84f2bd852dda8335c
ewinkler <winkler.em@gmail.com>
2016-10-29 12:30:34 -0400

    Cleaned up user load script

--- src/mds/core/management/commands/load_users.py
+++ src/mds/core/management/commands/load_users.py
@@ -5,11 +5,14 @@ from django.core.management.base import BaseCommand
 from mds.utils import csv_import
 
 class Command(BaseCommand):
-    args = '<full path to file>'
+    args = '<fname>'
     help = 'user csv list formatted as <command>,<username>,<password,villages|villages>. Username values must be unique.'
 
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+    
     def _load_users(self, fname):
         csv_import.load_users(fname)
 
     def handle(self, *args, **options):
-        self._load_locations(args[0])
+        self._load_users(args[0])
