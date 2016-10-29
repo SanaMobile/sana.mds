cc2b94411c11961577e88a2aadd3b1a5df64d25e
ewinkler <winkler.em@gmail.com>
2016-10-29 12:27:55 -0400

    Add test functionality

--- src/mds/core/management/commands/load_locations.py
+++ src/mds/core/management/commands/load_locations.py
@@ -4,11 +4,18 @@ from django.core.management.base import BaseCommand
 from mds.utils import csv_import
 
 class Command(BaseCommand):
-    args = '<full path to file>'
-    help = 'Loads location list formatted as code, name. Code values must be unique'
-
-    def _load_locations(self, fname):
-        csv_import.load_locations(fname)
+    args = '<fname>'
+    help = 'Loads location list formatted as <code>,<name>. <code> values must be unique'
+            
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+    
+    def _load_locations(self, fname, test=False):
+        csv_import.load_locations(fname, test=test)
 
     def handle(self, *args, **options):
-        self._load_locations(args[0])
+        try:
+            test = args[1]
+        except:
+            test=False
+        self._load_locations(args[0],test=test)
