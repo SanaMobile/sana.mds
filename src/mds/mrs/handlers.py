a0a155618966f7d1301564cbce42516c556c1d8e
ewinkler <winkler.em@gmail.com>
2017-09-28 14:09:31 -0400

    Removed redundant save calls

--- src/mds/mrs/handlers.py
+++ src/mds/mrs/handlers.py
@@ -115,10 +115,10 @@ class SavedProcedureHandler(BaseHandler):
             
             encounter.save()
             logging.debug("Saved encounter: " + encounter.uuid)
-            observations = responses_to_observations(encounter, data,sort=True)
+            observations = responses_to_observations(encounter, data,sort=False)
 	    
             for obs in observations:
-                obs.save()
+                # obs.save()
                 if obs.is_complex:
                     obs.create_file()
                 
